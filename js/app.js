66c63fff4b59d3bb688253f6b29033fb4b1e1ce3
doaa-1996 <doaaobeidat1996@gmail.com>
2021-02-07 15:47:51 +0200

    done

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var CustomerName = prompt('what is your name ?');
+var Address =prompt('your address ');
+var Phone =prompt('your phone number');
+var Order =prompt('select your order');
+
+
+if(Order='Pizza')
+{
+    var kind =prompt('what is the kind of pizza');
+
+}
+
+
+
+
+
+console.log( CustomerName);
+console.log( Address);
+console.log( Phone);
+console.log(Order );
+
+
+
+
+console.log('Thank you');
+document.write(<h1>Thank you</h1>);
+console.log('We recieved your order');
+document.write(We recieved your order);
+
+document.write(CustomerName +","+ Address+","+Phone+"," + Order );
+
+
+
+
