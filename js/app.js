e9ea6ad1152c518e64a6dff943d7fe9286b17f0f
doaa-1996 <doaaobeidat1996@gmail.com>
2021-02-08 13:41:43 +0200

    hhhhh

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-var CustomerName = prompt('what is your name ?');
-var Address =prompt('your address ');
-var Phone =prompt('your phone number');
+/*var CustomerName = prompt('what is your name ?');
 var Order =prompt('select your order');
 
 
@@ -11,23 +9,70 @@ if(Order='Pizza')
 }
 
 
-
-
-
 console.log( CustomerName);
-console.log( Address);
-console.log( Phone);
 console.log(Order );
+
+console.log('Thank you');
+document.write('<h1>Thank you</h1>');
+console.log('We recieved your order');
+document.write('We recieved your order');
+
+document.write(CustomerName +","+  Order );
+
+
+
+// today work
+
+*/
+
+var CustomerName = prompt('what is your name ?');
+var Order = prompt('select your order');
+
+
+while (Order !== "pizza" && Order !== "pasta")
+{
+    Order = prompt('select your order');
+}
+
+
+if (Order == "pizza") 
+{
+    var kind = prompt('what is the kind of pizza');
+}
+
+var itemOrder ='';
+if (Order == "pizza") {
+    itemOrder = '<img src="../pizza.jpg"/>';
+}
+else if (Order == "pasta") {
+    itemOrder = '<img src="../pasta.jpg"/>';
+}
+document.write(itemOrder);
+
+var num = prompt("What is the number of orders?")
+var result = '';
+
+
+
+document.write(result);
+
+
+
+console.log(CustomerName);
+console.log(Order);
 
 
 
 
 console.log('Thank you');
-document.write(<h1>Thank you</h1>);
+document.write('<h1>Thank you</h1>');
 console.log('We recieved your order');
-document.write(We recieved your order);
+document.write('<h1>We recieved your order</h1>');
 
-document.write(CustomerName +","+ Address+","+Phone+"," + Order );
+document.write(CustomerName + ","+num+";" + Order);
+
+
+
 
 
 
