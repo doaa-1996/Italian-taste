5498c8cea4b4b008088df51b7312dcd79d0a4ef8
doaa-1996 <doaaobeidat1996@gmail.com>
2021-02-09 13:09:23 +0200

    function

--- js/app.js
+++ js/app.js
@@ -1,44 +1,15 @@
-/*var CustomerName = prompt('what is your name ?');
-var Order =prompt('select your order');
-
-
-if(Order='Pizza')
-{
-    var kind =prompt('what is the kind of pizza');
-
-}
-
-
-console.log( CustomerName);
-console.log(Order );
-
-console.log('Thank you');
-document.write('<h1>Thank you</h1>');
-console.log('We recieved your order');
-document.write('We recieved your order');
-
-document.write(CustomerName +","+  Order );
-
-
-
-// today work
-
-*/
-
 var CustomerName = prompt('what is your name ?');
 var Order = prompt('select your order');
-
 
 while (Order !== "pizza" && Order !== "pasta")
 {
     Order = prompt('select your order');
 }
 
-
 if (Order == "pizza") 
-{
-    var kind = prompt('what is the kind of pizza');
-}
+
+var kind = prompt('what is kind of pizza');
+
 
 var itemOrder ='';
 if (Order == "pizza") {
@@ -51,27 +22,20 @@ document.write(itemOrder);
 
 var num = prompt("What is the number of orders?")
 var result = '';
-
-
-
 document.write(result);
-
-
-
 console.log(CustomerName);
 console.log(Order);
-
-
-
-
 console.log('Thank you');
-document.write('<h1>Thank you</h1>');
-console.log('We recieved your order');
-document.write('<h1>We recieved your order</h1>');
+function orderDone(){
+    document.write('<h1>Thank you</h1>');
+    console.log('We recieved your order');
+    document.write('<h1>We recieved your order</h1>');
+}
+orderDone();
 
 document.write(CustomerName + ","+num+";" + Order);
 
-
+//alert('hello from the new updates')
 
 
 
